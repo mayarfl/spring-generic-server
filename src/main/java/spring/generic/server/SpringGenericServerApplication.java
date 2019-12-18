376ce6ab565f115d58cca64e492536810146c11f
mayar <mayar@microfocus.com>
2019-12-18 15:01:43 +0200

    another test

--- src/main/java/spring/generic/server/SpringGenericServerApplication.java
+++ src/main/java/spring/generic/server/SpringGenericServerApplication.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class SpringGenericServerApplication {
+	public static String PASSWORD = "mypassword";
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGenericServerApplication.class, args);
+		String[] args1 = new String[args.length + 1];
+		args1[args.length] = PASSWORD;
+		SpringApplication.run(SpringGenericServerApplication.class, args1);
 	}
 }
