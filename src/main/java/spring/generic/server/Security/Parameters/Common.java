3d1e4a27624c4a78fbba034e51630db5ddd8481b
mayar <mayar@microfocus.com>
2019-12-23 15:50:57 +0200

    another test

--- src/main/java/spring/generic/server/Security/Parameters/Common.java
+++ src/main/java/spring/generic/server/Security/Parameters/Common.java
@@ -3,4 +3,6 @@ package spring.generic.server.Security.Parameters;
 public class Common {
     public static final String USERNAME = "username";
     public static final String PASSWORD = "password";
+
+
 }
---
+++ src/main/java/spring/generic/server/Security/Parameters/User.java
@@ -0,0 +1,28 @@
+package spring.generic.server.Security.Parameters;
+
+public class User {
+        public static final String NAME = "ROLE_SIMPLE";
+        public static final String PASSWORD = "ROLE_ADMIN";
+
+    public class User1 {
+        public static final String NAME = "ROLE_SIMPLE";
+        public static final String PASSWORD = "ROLE_ADMIN";
+    }
+
+    public class newUser {
+        public static final String NAME = "ROLE_SIMPLE";
+        public static final String PASSWORD = "ROLE_ADMIN";
+    }
+
+    public class otherUser {
+        public static final String NAME = "ROLE_SIMPLE";
+        public static final String PASSWORD = "ROLE_ADMIN";
+    }
+
+    public class oldUser {
+        public static final String NAME = "ROLE_SIMPLE";
+        public static final String PASSWORD = "ROLE_ADMIN";
+    }
+
+}
+
