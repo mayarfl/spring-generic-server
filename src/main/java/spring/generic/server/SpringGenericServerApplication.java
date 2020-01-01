d534161f66059aa163bd58a96f047f65a09163d2
mayar <mayar@microfocus.com>
2020-01-01 15:52:28 +0200

    another test

--- src/main/java/spring/generic/server/SpringGenericServerApplication.java
+++ src/main/java/spring/generic/server/SpringGenericServerApplication.java
@@ -28,6 +28,17 @@ public class SpringGenericServerApplication {
 		public static final String PASSWORD = "ROLE_ADMIN";
 	}
 
+	public class User3 {
+		public static final String NAME = "ROLE_SIMPLE";
+		public static final String PASSWORD = "ROLE_ADMIN";
+	}
+
+	public class User4 {
+		public static final String NAME = "ROLE_SIMPLE";
+		public static final String PASSWORD = "ROLE_ADMIN";
+	}
+
+
 	public static void main(String[] args) {
 		String[] args1 = new String[args.length + 4];
 		args1[args.length] = PASSWORD;
