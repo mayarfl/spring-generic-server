acc88474abc6ad0d15a63ef3135a6b0ae34340f9
mayar <mayar@microfocus.com>
2020-01-06 16:24:58 +0200

    another test

--- src/main/java/spring/generic/server/SpringGenericServerApplication.java
+++ src/main/java/spring/generic/server/SpringGenericServerApplication.java
@@ -28,64 +28,12 @@ public class SpringGenericServerApplication {
 		public static final String PASSWORD = "ROLE_ADMIN";
 	}
 
-	public class User3 {
+	public class User15 {
 		public static final String NAME = "ROLE_SIMPLE";
 		public static final String PASSWORD = "ROLE_ADMIN";
 	}
 
-	public class User4 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User5 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User6 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-
-	public class User7 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User8 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-
-	public class User9 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User10 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User11 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User12 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User13 {
-		public static final String NAME = "ROLE_SIMPLE";
-		public static final String PASSWORD = "ROLE_ADMIN";
-	}
-
-	public class User14 {
+	public class User16 {
 		public static final String NAME = "ROLE_SIMPLE";
 		public static final String PASSWORD = "ROLE_ADMIN";
 	}
