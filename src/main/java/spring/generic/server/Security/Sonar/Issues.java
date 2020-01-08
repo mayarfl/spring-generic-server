84ed8abb39cf75681a446834b65568bb184b842e
mayar <mayar@microfocus.com>
2020-01-08 14:04:57 +0200

    another test

--- src/main/java/spring/generic/server/Security/Sonar/Issues.java
+++ src/main/java/spring/generic/server/Security/Sonar/Issues.java
@@ -44,4 +44,27 @@ public class Issues {
 
         return jsonText;
     }
+
+    public String getStream1(File path) {
+        InputStream is = null;
+        try {
+            String str = String.valueOf(path);
+            str = str.replace("password", "PASSWORD");
+            is = new FileInputStream(String.valueOf(path));
+
+
+        } catch (FileNotFoundException e) {
+            // logger.log(String.format("File path not found %s", e.getMessage()));
+        }
+
+        String jsonText = "";
+        try {
+            jsonText = IOUtils.toString(is, String.valueOf(StandardCharsets.UTF_8));
+            String str1 = jsonText.replace("password", "PASSWORD");
+        } catch (IOException e) {
+            //    logger.log(String.format("Failed to create the json object %s", e.getMessage()));
+        }
+
+        return jsonText;
+    }
 }
--- src/main/java/spring/generic/server/Security/Sonar/NewIssue.java
+++ src/main/java/spring/generic/server/Security/Sonar/NewIssue.java
@@ -1,5 +1,33 @@
 package spring.generic.server.Security.Sonar;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class NewIssue {
     public String password = "password";
+
+    public String getStream(File path) {
+        InputStream is = null;
+        try {
+            String str = String.valueOf(path);
+            str = str.replace("password", "PASSWORD");
+            is = new FileInputStream(String.valueOf(path));
+
+
+        } catch (FileNotFoundException e) {
+            // logger.log(String.format("File path not found %s", e.getMessage()));
+        }
+
+        String jsonText = "";
+        try {
+            jsonText = IOUtils.toString(is, String.valueOf(StandardCharsets.UTF_8));
+            String str1 = jsonText.replace("password", "PASSWORD");
+        } catch (IOException e) {
+            //    logger.log(String.format("Failed to create the json object %s", e.getMessage()));
+        }
+
+        return jsonText;
+    }
 }
