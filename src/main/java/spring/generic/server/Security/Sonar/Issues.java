43aeae0f17009f98353a271be9844d6871f64313
mayar <mayar@microfocus.com>
2020-04-07 15:08:32 +0300

    quality story #1003, revert

--- src/main/java/spring/generic/server/Security/Sonar/Issues.java
+++ src/main/java/spring/generic/server/Security/Sonar/Issues.java
@@ -32,7 +32,7 @@ public class Issues {
 
 
         } catch (FileNotFoundException e) {
-            logger.log(String.format("File path not found %s", e.getMessage()));
+            // logger.log(String.format("File path not found %s", e.getMessage()));
         }
 
         String jsonText = "";
