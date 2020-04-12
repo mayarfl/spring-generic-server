b7f3b19ed2df8ff74b7f1d006b23c3093ccbef00
mayar <mayar@microfocus.com>
2020-04-12 15:36:59 +0300

    quality story #1005, feature

--- src/main/java/spring/generic/server/Security/Sonar/PreciseIssue.java
+++ src/main/java/spring/generic/server/Security/Sonar/PreciseIssue.java
@@ -67,4 +67,10 @@ public class PreciseIssue {
         public static final String NAME = "ROLE_SIMPLE";
         public static final String PASSWORD = "ROLE_ADMIN";
     }
+
+    public class User15 {
+        public static final String NAME = "ROLE_SIMPLE";
+        public static final String PASSWORD = "ROLE_ADMIN";
+    }
+
 }
