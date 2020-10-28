55a7bf9290c68e562c7e456191572244405f9751
mayar <mayar@microfocus.com>
2020-10-28 14:44:19 +0200

    user story #1052 description

--- src/test/java/spring/generic/server/SpringGenericServerApplicationTests.java
+++ src/test/java/spring/generic/server/SpringGenericServerApplicationTests.java
@@ -9,8 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 //@SpringBootTest
 public class SpringGenericServerApplicationTests {
 
-
-
 //	@Test
 //	public void contextLoads() {
 //	}
